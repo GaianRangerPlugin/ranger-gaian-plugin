429695ce86dec8f31e13380b504500ff4b05b005
Nigel Jones <nigel.l.jones@gmail.com>
2018-02-27 14:24:36 +0100

    Further updates to dummy ranger resource lookup

--- plugin/src/main/java/org/apache/ranger/services/gaian/RangerServiceGaian.java
+++ plugin/src/main/java/org/apache/ranger/services/gaian/RangerServiceGaian.java
@@ -1,4 +1,17 @@
-package java.org.apache.ranger.services.gaian;
+package org.apache.ranger.services.gaian;
+import java.util.HashMap;
+import java.util.List;
 
-public class RangerServiceGaian {
+import org.apache.ranger.plugin.service.RangerBaseService;
+import org.apache.ranger.plugin.service.ResourceLookupContext;
+
+public class RangerServiceGaian extends RangerBaseService {
+    public HashMap<String, Object> validateConfig() throws Exception {
+        // TODO: connect to Gaian resource manager; throw Exception on failure
+        return null;
+    }
+    public List<String> lookupResource(ResourceLookupContext context) throws Exception {
+        // TODO: retrieve the resource list from Gaian resource manager using REST API
+        return null;
+    }
 }
